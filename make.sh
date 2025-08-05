7336035fe1c74d16642ae1d48882a5676efbc6d4
josdugan <josephdug@gmail.com>
2025-08-04 22:29:33 -0400

    Update make script.

--- make.sh
+++ make.sh
@@ -4,5 +4,5 @@ echo "Building API Docs"
 
 # Bundle the API docs
 
-npx @redocly/cli bundle -o apifirst.yaml && \
+npx @redocly/cli bundle ./openapi/openapi.yaml -o apifirst.yaml && \
 npx @redocly/cli build-docs apifirst.yaml -o index.html
